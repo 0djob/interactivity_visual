7ed42bef46dc715bac564e4adadfd7e7d704e2bc
Finn Ackerman <fra26@drexel.edu>
2025-11-06 02:15:11 -0500

    Configure frontend with live Render backend URL

--- game_studio_frontend/build/web/flutter_bootstrap.js
+++ game_studio_frontend/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3595882997"
+    serviceWorkerVersion: "2158775001"
   }
 });
